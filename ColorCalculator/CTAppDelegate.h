36f22aa97628e9a44441b49c8722dfc2005df0aa
cj1024 <ferdinand31@sohu.com>
2013-12-18 16:08:32 +0800

    Initial Commit

---
+++ ColorCalculator/CTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTAppDelegate.h
+//  ColorCalculator
+//
+//  Created by j_chen on 13-12-18.
+//  Copyright (c) 2013年 j_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ColorCalculator
+//
+//  Created by j_chen on 13-12-18.
+//  Copyright (c) 2013年 j_chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CTAppDelegate class]));
+    }
+}
